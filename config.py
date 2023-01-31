a44e908433ea0cdc5042cf14f937ac47b848ad28
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-31 15:58:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6007861522:AAFnbVI_PLo5C1eDjddTfpxxpOFjeFt_xKo"
+    BOT_TOKEN = "6032076608:AAHGXUrmFaf2_BUtoS5EyGm_SAuOhhfEzAg"
     # The Telegram API things
     API_ID = "17983098"
     API_HASH = "ee28199396e0925f1f44d945ac174f64"
@@ -10,15 +10,15 @@ class Config(object):
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
+    MAX_FILE_SIZE = 4194304000
     TG_MAX_FILE_SIZE = 4194304000 #2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
+    FREE_USER_MAX_FILE_SIZE = 4194304000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(128)
     # default thumbnail to be used in the videos
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = "64.227.148.231:1994"
+    HTTP_PROXY = "147.28.182.125:3128"
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
